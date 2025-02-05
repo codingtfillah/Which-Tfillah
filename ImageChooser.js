28dd1f8a8697dbfde207885f7566078ad1113f2b
codingtfillah <orenk@jcdsboston.org>
2025-02-05 12:29:27 -0500

    Update ImageChooser.js

--- ImageChooser.js
+++ ImageChooser.js
@@ -1,25 +1,25 @@
 import {idDict} from "./IdDict.js"
 
-const randint = (start, end) => {
-    return Math.floor(Math.random() * (end - start + 1) + start)
-}
-
 const choicesCount = 4
 
-let choices = []
-for (let i = 0; i < choicesCount; i++) {
-    const dictLength = Object.keys(idDict).length
-    if (i < dictLength) {
-        let nextChoice = `id-${randint(1, dictLength)}`
-        while (choices.includes(nextChoice)) {
-            nextChoice = `id-${randint(1, dictLength)}`
-        }
-        choices.push(nextChoice)
-    }
-}
-
-const correctAnswer = choices[randint(0, choices.length-1)]
-
 export const getChoices = () => {
+    const randint = (start, end) => {
+        return Math.floor(Math.random() * (end - start + 1) + start)
+    }
+    
+    let choices = []
+    for (let i = 0; i < choicesCount; i++) {
+        const dictLength = Object.keys(idDict).length
+        if (i < dictLength) {
+            let nextChoice = `id-${randint(1, dictLength)}`
+            while (choices.includes(nextChoice)) {
+                nextChoice = `id-${randint(1, dictLength)}`
+            }
+            choices.push(nextChoice)
+        }
+    }
+    
+    const correctAnswer = choices[randint(0, choices.length-1)]
+
     return [correctAnswer, choices]
 }
