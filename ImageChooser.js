375d432e096798604516b8d3302645538c9ac515
codingtfillah <orenk@jcdsboston.org>
2025-02-05 11:57:55 -0500

    Update ImageChooser.js

--- ImageChooser.js
+++ ImageChooser.js
@@ -4,13 +4,13 @@ const randint = (start, end) => {
     return Math.floor(Math.random() * (end - start + 1) + start)
 }
 
-choicesCount = 4
+const choicesCount = 4
 
-choices = []
+let choices = []
 for (let i = 0; i < choicesCount; i++) {
-    dictLength = Object.keys(idDict).length
+    const dictLength = Object.keys(idDict).length
     if (i < dictLength) {
-        nextChoice = `id-${randint(1, dictLength)}`
+        let nextChoice = `id-${randint(1, dictLength)}`
         while (choices.includes(nextChoice)) {
             nextChoice = `id-${randint(1, dictLength)}`
         }
@@ -18,7 +18,6 @@ for (let i = 0; i < choicesCount; i++) {
     }
 }
 
-correct_answer = choices[randint(0, choices.length-1)]
+const correct_answer = choices[randint(0, choices.length-1)]
 
-export {correct_answer}
-export {choices}
+export {correct_answer, choices}
