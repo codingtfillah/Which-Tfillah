009719aae53313aee4be9e4b4049d253616a7a8b
codingtfillah <orenk@jcdsboston.org>
2025-02-05 11:45:20 -0500

    Create ImageChooser.js

---
+++ ImageChooser.js
@@ -0,0 +1,28 @@
+const randint = (start, end) => {
+    return Math.floor(Math.random() * (end - start + 1) + start)
+}
+
+choicesCount = 4
+
+myDict = {
+    "id-1" : "shema",
+    "id-2" : "ahava raba",
+    "id-3" : "modeh ani",
+    "id-4" : "amidah",
+    "id-5" : "halelu",
+    "id-6" : "ashrei",
+}
+
+choices = []
+for (let i = 0; i < choicesCount; i++) {
+    dictLength = Object.keys(myDict).length
+    if (i < dictLength) {
+        nextChoice = `id-${randint(1, dictLength)}`
+        while (choices.includes(nextChoice)) {
+            nextChoice = `id-${randint(1, dictLength)}`
+        }
+        choices.push(nextChoice)
+    }
+}
+
+correct_answer = choices[randint(0, choices.length-1)]
